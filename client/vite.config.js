7a42363c3c75c4c6ceaad0bbb108a6093beb0743
Pratik Shah <pratik.ashah676@gmail.com>
2024-08-24 18:31:06 +0530

    Add build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: '.', // This tells Vite that the root is the current directory (client)
   plugins: [react()],
-  base: '/', // Adjust this if your app is not at the root
+  build: {
+    outDir: 'dist', // Output directory will be 'client/dist'
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // Entry point to your app
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // Optional: alias for easier imports
+    },
+  },
 })
