44b02b87d3b6392b9e5eb78482b1ceb2a7e92214
Pratik Shah <pratik.ashah676@gmail.com>
2024-08-24 17:30:11 +0530

    Add Protected

--- client/src/Components/Reuseable/Protected.jsx
+++ client/src/Components/Reuseable/Protected.jsx
@@ -1,23 +1,19 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const Protected = ({children}) => {
-  const {token , user } = useSelector(state => state.auth)
-  const navigate = useNavigate()
+const Protected = ({ children }) => {
+  const { token, user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   useEffect(() => {
-    if(!token || !user){
-        navigate("/")
+    if (!token || !user) {
+      navigate("/");
     }
-    if(!token || !user){
-        return null;
+    if (!token || !user) {
+      return null;
     }
-  },[])
-  return (
-      { 
-        children
-      }
-  )
-}
+  }, []);
+  return children;
+};
 
-export default Protected
+export default Protected;
